b7032bb2b56bddea13f339a2053dc804af5a0c8e
Aaron <aaronmaosa@sina.com>
2024-05-26 20:03:09 +0800

    Create 4399弹窗关闭.user.js

---
+++ program/Tampermonkey/4399弹窗关闭.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         4399弹窗关闭
+// @namespace    https://aaron888hacker.github.io/program/Tampermonkey/#4399弹窗
+// @version      0.0.2
+// @description  关闭4399登录游戏弹窗，畅玩无需登录的小游戏
+// @author       aaron888hacker
+// @match        *://www.4399.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    //document.getElementsByClassName("fcmdialog")[0].style.display = "none"
+    //document.getElementsByClassName("fcmdialog")[0].style.display = "cmMask"
+    function a(){Anti_close();}
+    setTimeout(a, 1000)
+
+})();
